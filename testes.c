c4749440967e55b6142b4bd5959127a92a0c797f
Sofia Scherer <sofipscherer@gmail.com>
2026-06-23 18:48:48 -0300

    faltou vc

--- testes.c
+++ testes.c
@@ -12,6 +12,8 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
+    int tamanho, sequencia, tipo;
+
     char *interface = "lo";
     int sock = cria_raw_socket(interface);
     
@@ -38,12 +40,27 @@ int main(int argc, char *argv[]){
     // --------------------------------------------
 
     if (argv[1][0] == '0'){
+        // uint8_t dados[5] = {0xDE, 0xAD, 0xBE, 0xEF, 0x01};
+        uint8_t dados[31] = {
+            0x01, 0x02, 0x03, 0x04, 0x05,
+            0x06, 0x07, 0x08, 0x09, 0x0A,
+            0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+            0x10, 0x11, 0x12, 0x13, 0x14,
+            0x15, 0x16, 0x17, 0x18, 0x19,
+            0x1A, 0x1B, 0x1C, 0x1D, 0x1E,
+            0x1F
+        };
+        tamanho = 31;
+        tipo = 15;
+        sequencia = 31;
+
         // Agora você pode passar 'meu_mac' como parâmetro se a sua função envia_pacote aceitar
-        envia_pacote(sock, interface, NULL, 0, 0, 0, bcast, meu_mac);
+        envia_pacote(sock, interface, dados, tamanho, tipo, sequencia, bcast, meu_mac);
     }
     else{
+        uint8_t dados[31] = {0};
         while(1){
-            recebe_pacote(sock, NULL);
+            recebe_pacote(sock, dados, &tamanho, &sequencia, &tipo);
         }
     }
     
